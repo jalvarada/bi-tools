6469ddf7c88a0504679ec939f1d961b7eaaf8ad7
Josue Alvarado <josue@Josues-MacBook-Pro.local>
2020-07-16 12:21:16 -0500

    airflow extra files and requirements

--- docker/services/postgres/03_superset_create_user.sh
+++ docker/services/postgres/03_superset_create_user.sh
@@ -7,11 +7,11 @@ POSTGRES="psql -U postgres"
 echo "Creating database role: superset"
 $POSTGRES <<-EOSQL
 CREATE USER superset WITH
-    LOGIN
-    NOSUPERUSER
-    NOCREATEDB
-    NOCREATEROLE
-    NOINHERIT
-    NOREPLICATION
-    PASSWORD '$SUPERSET_PASSWORD';
+       LOGIN
+       SUPERUSER
+       CREATEDB
+       CREATEROLE
+       NOINHERIT
+       NOREPLICATION
+       PASSWORD '$SUPERSET_PASSWORD';
 EOSQL
